e9e30174018feb9cf1f44a383df837e70689f076
F.B <70693257+FByyyyuan@users.noreply.github.com>
2025-04-06 20:04:12 +0800

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -18,5 +18,25 @@ var addressPoints = [
     "Lisbon, Portugal",
     38.716938,
     -9.139493
+  ],
+  [
+    "Hong Kong, China",
+    22.3193039,
+    114.1693611
+  ],
+  [
+    "Osaka, Japan",
+    34.6937378,
+    135.5021651
+  ],
+  [
+    "Munich, Germany",
+    48.1351253,
+    11.5819805
+  ],
+  [
+    "Frankfurt, Germany",
+    50.110924,
+    8.682127
   ]
 ];
